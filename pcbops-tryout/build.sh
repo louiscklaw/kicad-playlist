edc5f4559704377762d8d4373b98ca7707c89de3
louiscklaw <louiscklaw@gmail.com>
2020-04-09 15:55:57 +0800

    ci: init helloworld,

---
+++ pcbops-tryout/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+docker run -t -v $PWD:/workspace  akshmakov/pcb-cicd make ci_bom
+docker run -t -v $PWD:/workspace  akshmakov/pcb-cicd make ci_drawings
+docker run -t -v $PWD:/workspace  akshmakov/pcb-cicd make ci_drc
+docker run -t -v $PWD:/workspace  akshmakov/pcb-cicd make ci_erc
+docker run -t -v $PWD:/workspace  akshmakov/pcb-cicd make ci_plot
+docker run -t -v $PWD:/workspace  akshmakov/pcb-cicd make ci_pnp
+docker run -t -v $PWD:/workspace  akshmakov/pcb-cicd make ci_rfq
+docker run -t -v $PWD:/workspace  akshmakov/pcb-cicd make ci_sch
+docker run -t -v $PWD:/workspace  akshmakov/pcb-cicd make ci_step
