545fa295ce217e46f38ffcad5a953d97fe266c31
louiscklaw <louiscklaw@gmail.com>
2020-08-19 01:34:38 +0800

    init update and test,

--- scripts/new_from.sh
+++ scripts/new_from.sh
@@ -9,3 +9,12 @@ git branch -D poc/$TEST
 git checkout -b poc/$TEST
 
 rsync -avz ../$1/ .
+
+cd ./kicad
+
+  mv *.kicad_pcb $TEST.kicad_pcb
+  mv *.sch $TEST.sch
+  mv *.pro $TEST.pro
+  mv *-cache.lib $TEST-cache.lib
+
+cd -
