0cbdff8b676603084b3a37d84eeede6d8edc33a9
louiscklaw <louiscklaw@gmail.com>
2020-08-21 16:18:26 +0800

    update,

--- 7_by_9_breadboard-tryout/build.sh
+++ 7_by_9_breadboard-tryout/build.sh
@@ -8,10 +8,14 @@ docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_bom   &
 docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_drawings   &
 docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_drc   &
 docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_erc   &
-docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_plot   &
 docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_pnp   &
 docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_rfq   &
 docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_sch   &
 docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_step   &
+
+docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_plot   &
+docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_plot_gerber   &
+docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_plot_pdf   &
+docker run -t -v $PWD:/workspace  logickee/pcb-cicd make ci_plot_svg   &
 
 wait
