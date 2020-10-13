42b74ab3a77573af48165f0c70f794e2cc28a5d7
louiscklaw <louiscklaw@gmail.com>
2020-10-13 21:12:31 +0800

    update,

--- library/build.sh
+++ library/build.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 cd symbols/taobao-c
+  ./build.sh
+cd -
+
+cd symbols/taobao-r
+  ./build.sh
+cd -
+
+cd symbols/taobao-l
   ./build.sh
 cd -
--- library/symbols/taobao-r/build.sh
+++ library/symbols/taobao-r/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3 ./gen_c.py
+python3 ./gen_r.py
