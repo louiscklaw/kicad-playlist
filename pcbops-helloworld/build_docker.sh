17566e45be8c9d607e127cc9ed62d150547bc901
louiscklaw <louiscklaw@gmail.com>
2020-11-12 00:36:02 +0800

    init commit,

--- pcbops-helloworld/build_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-docker build . -t logickee/pcb-cicd
--- pcbops-helloworld/scripts/common.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def maybe_create_dir(output_dir):
-    try:
-        os.mkdir(output_dir)
-    except OSError:
-        pass
-    
